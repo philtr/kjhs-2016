cbe24c562b86c4a5153dbdef556c22a1b953272c
Phillip Ridlen <p@rdln.net>
2016-05-19 22:31:46 -0500

    Add school subjects slide

---
+++ slides/004-subjects.rb
@@ -0,0 +1,20 @@
+
+class Developer < Person
+  def subjects_to_study
+    [
+      "Computer Science", # Duh
+      "Math",
+      "Art",
+      "English", # yes, English!
+    ]
+  end
+end
+
+
+
+
+
+
+
+
+
